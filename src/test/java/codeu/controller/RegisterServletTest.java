6d2dc1d788431f7cfce5cc92e0f67ff51d978f02
DESKTOP-BSFLR4J\Bakuano <sordcpt.blakburger1@gmail.com>
2018-03-19 00:31:49 -0400

    Project1-5 Handle POST in RegisterServlet. Also added test methods

--- src/test/java/codeu/controller/RegisterServletTest.java
+++ src/test/java/codeu/controller/RegisterServletTest.java
@@ -1,35 +1,77 @@
 package codeu.controller;
 
 import java.io.IOException;
+import java.io.StringWriter;
+import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.mockito.ArgumentCaptor;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import codeu.model.data.User;
+import codeu.model.store.basic.UserStore;
 
 public class RegisterServletTest {
 
- private RegisterServlet registerServlet;
- private HttpServletRequest mockRequest;
- private HttpServletResponse mockResponse;
- private RequestDispatcher mockRequestDispatcher;
+  private RegisterServlet registerServlet;
+  private HttpServletRequest mockRequest;
+  private HttpServletResponse mockResponse;
+  private RequestDispatcher mockRequestDispatcher;
 
- @Before
- public void setup() {
-   registerServlet = new RegisterServlet();
-   mockRequest = Mockito.mock(HttpServletRequest.class);
-   mockResponse = Mockito.mock(HttpServletResponse.class);
-   mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
-   Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/register.jsp"))
-       .thenReturn(mockRequestDispatcher);
- }
+  @Before
+  public void setup() {
+    registerServlet = new RegisterServlet();
+    mockRequest = Mockito.mock(HttpServletRequest.class);
+    mockResponse = Mockito.mock(HttpServletResponse.class);
+    mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
+    Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/register.jsp")).thenReturn(mockRequestDispatcher);
+  }
 
- @Test
- public void testDoGet() throws IOException, ServletException {
-   registerServlet.doGet(mockRequest, mockResponse);
+  @Test
+  public void testDoGet() throws IOException, ServletException {
+    registerServlet.doGet(mockRequest, mockResponse);
 
-   Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
- }
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  }
+
+  @Test
+  public void testDoPost_BadUsername() throws IOException, ServletException {
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("bad !@#$% username");
+
+    registerServlet.doPost(mockRequest, mockResponse);
+
+    Mockito.verify(mockRequest).setAttribute("error", "Please enter only letters, numbers, and spaces.");
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  }
+
+  @Test
+  public void testDoPost_NewUser() throws IOException, ServletException {
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("Testertest");
+    Mockito.when(mockRequest.getParameter("password")).thenReturn("password");
+
+    StringWriter strWriter = new StringWriter();
+    PrintWriter priWriter = new PrintWriter(strWriter);
+    Mockito.when(mockResponse.getWriter()).thenReturn(priWriter);
+
+    UserStore mockUserStore = Mockito.mock(UserStore.class);
+    Mockito.when(mockUserStore.isUserRegistered("Testertest")).thenReturn(false);
+    registerServlet.setUserStore(mockUserStore);
+
+    HttpSession mockSession = Mockito.mock(HttpSession.class);
+    Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+
+    registerServlet.doPost(mockRequest, mockResponse);
+
+    ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+
+    Mockito.verify(mockUserStore).addUser(userArgumentCaptor.capture());
+    Assert.assertEquals(userArgumentCaptor.getValue().getName(), "Testertest");
+
+    Mockito.verify(mockResponse).sendRedirect("/login");
+  }
 }
