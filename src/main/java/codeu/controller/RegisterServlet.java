06c8f11c546d9c73363a526edc0dd3b007df5d37
DESKTOP-BSFLR4J\Bakuano <sordcpt.blakburger1@gmail.com>
2018-03-19 20:52:52 -0400

    cleanup of java comment asterisks. pushed one space right

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -16,14 +16,14 @@ import java.time.Instant;
 public class RegisterServlet extends HttpServlet {
 
   /**
-  * Store class that gives access to Users.
-  */
+   * Store class that gives access to Users.
+   */
   private UserStore userStore;
 
   /**
-  * Set up state for handling registration-related requests. This method is only called when
-  * running in a server, not when running in a test.
-  */
+   * Set up state for handling registration-related requests. This method is only called when
+   * running in a server, not when running in a test.
+   */
   @Override
   public void init() throws ServletException {
     super.init();
@@ -31,21 +31,23 @@ public class RegisterServlet extends HttpServlet {
   }
 
   /**
-  * Sets the UserStore used by this servlet. This function provides a common setup method
-  * for use by the test framework or the servlet's init() function.
-  */
+   * Sets the UserStore used by this servlet. This function provides a common setup method
+   * for use by the test framework or the servlet's init() function.
+   */
   void setUserStore(UserStore userStore) {
     this.userStore = userStore;
   }
 
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException { 
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws IOException, ServletException { 
     request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
   }
 
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) 
+    throws IOException, ServletException {
     String username = request.getParameter("username");
     String password = request.getParameter("password");
 
