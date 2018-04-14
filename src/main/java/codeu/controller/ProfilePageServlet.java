7cde4e2c84478d77980fdc8a3a79352570957321
DESKTOP-BSFLR4J\Bakuano <sordcpt.blakburger1@gmail.com>
2018-04-14 13:58:56 -0400

    Added UserProfileStore to ProfilePageServlet and updated methods

--- src/main/java/codeu/controller/ProfilePageServlet.java
+++ src/main/java/codeu/controller/ProfilePageServlet.java
@@ -1,5 +1,7 @@
 package codeu.controller;
 
+import codeu.model.store.basic.UserStore;
+import codeu.model.store.basic.UserProfileStore;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -7,14 +9,58 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet class responsible for User profile pages.
+ * Servlet class responsible for User Profile pages.
  */
 public class ProfilePageServlet extends HttpServlet {
 
+  /** Store class that gives access to Users and User Profiles. */
+  private UserStore userStore;
+  private UserProfileStore userProfileStore;
+
+  /**
+   * Set up state for handling profilepage-related requests. This method is only called when running in a
+   * server, not when running in a test.
+   */
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    setUserStore(UserStore.getInstance());
+    setUserProfileStore(UserProfileStore.getInstance());
+  }
+
+  /**
+   * Sets the UserStore used by this servlet. This function provides a common setup method for use
+   * by the test framework or the servlet's init() function.
+   */
+  void setUserStore(UserStore userStore) {
+    this.userStore = userStore;
+  }
+
+  /**
+   * Sets the UserProfileStore used by this servlet. This function provides a common setup method for use
+   * by the test framework or the servlet's init() function.
+   */
+  void setUserProfileStore(UserProfileStore userProfileStore) {
+    this.userProfileStore = userProfileStore;
+  }
+
+  /**
+   * This function fires when a user requests the /profilepage URL. It simply forwards the request to
+   * profilepage.jsp.
+   */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
     throws IOException, ServletException { 
     request.getRequestDispatcher("/WEB-INF/view/profilepage.jsp").forward(request, response);
   }
 
+  /**
+   * This function fires when a user posts the /profilepage URL. It simply forwards the request to
+   * profilepage.jsp.
+   */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException {
+
+    } 
 }
