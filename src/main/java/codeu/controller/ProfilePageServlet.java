8f1cb7ce7aa45f21db26780c0b886a9a29559100
DESKTOP-BSFLR4J\Bakuano <sordcpt.blakburger1@gmail.com>
2018-04-21 16:23:13 -0400

    profilePage doPost method implemented for saving all changes

--- src/main/java/codeu/controller/ProfilePageServlet.java
+++ src/main/java/codeu/controller/ProfilePageServlet.java
@@ -1,8 +1,12 @@
 package codeu.controller;
 
+import codeu.model.data.User;
+import codeu.model.data.UserProfile;
 import codeu.model.store.basic.UserStore;
 import codeu.model.store.basic.UserProfileStore;
+import java.time.Instant;
 import java.io.IOException;
+import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,7 +34,7 @@ public class ProfilePageServlet extends HttpServlet {
 
   /**
    * Sets the UserStore used by this servlet. This function provides a common setup method for use
-   * by the test framework or the servlet's init() function.
+   * by the test framework or the servlet's init() function.  
    */
   void setUserStore(UserStore userStore) {
     this.userStore = userStore;
@@ -71,5 +75,29 @@ public class ProfilePageServlet extends HttpServlet {
        * Send AboutMe, and Interests data to UserProfile object
        * Update UserProfileStore with new UserProfile object. 
        */
+      String username = (String) request.getSession().getAttribute("user");
+      if (userStore.isUserRegistered(username)) {
+        User user = userStore.getUser(username);
+        UserProfile userProfile = null;
+        String aboutMe = request.getParameter("aboutMe");
+        String profilePicture = request.getParameter("profilepicture");
+        String category = request.getParameter("myFavorites");
+        String subCategory = request.getParameter("subcategory");
+
+        if (userProfileStore.isUserProfileCreated(user.getId())) {
+          userProfile = userProfileStore.getUserProfile(user.getId());
+          userProfile.setLastTimeOnline(Instant.now());
+        }
+        else {
+          userProfile = new UserProfile(user.getId(), "", "", new HashMap<String, String>(), Instant.now());
+        }
+
+        userProfile.setAboutMe(aboutMe);
+        userProfile.setProfilePicture(profilePicture);
+        userProfile.addInterest(category, subCategory);
+
+        userProfileStore.addUserProfile(userProfile);
+        request.getRequestDispatcher("/WEB-INF/view/profilepage.jsp").forward(request, response);
+      }
     } 
 }
