2ce4bc31dd6fe59bcd9f01c63a272673db692783
Caitlin Schmuck <ces131@pitt.edu>
2018-04-23 19:50:56 -0400

    doGet of Profile page updated, registerServlet now includes profile creation, tests updated

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -6,7 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import codeu.model.data.User;
+import codeu.model.data.UserProfile;
 import codeu.model.store.basic.UserStore;
+import codeu.model.store.basic.UserProfileStore;
 import java.util.UUID;
 import java.time.Instant;
 import org.mindrot.jbcrypt.BCrypt;
@@ -21,6 +23,7 @@ public class RegisterServlet extends HttpServlet {
    * Store class that gives access to Users.
    */
   private UserStore userStore;
+  private UserProfileStore profileStore;
 
   /**
    * Set up state for handling registration-related requests. This method is only called when
@@ -30,6 +33,7 @@ public class RegisterServlet extends HttpServlet {
   public void init() throws ServletException {
     super.init();
     setUserStore(UserStore.getInstance());
+    setUserProfileStore(UserProfileStore.getInstance());
   }
 
   /**
@@ -40,6 +44,14 @@ public class RegisterServlet extends HttpServlet {
     this.userStore = userStore;
   }
 
+  /**
+   * Sets the UserProfileStore used by this servlet. This function provides a common setup method
+   * for use by the test framework or the servlet's init() function.
+   */
+
+  void setUserProfileStore(UserProfileStore profileStore) {
+    this.profileStore = profileStore;
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
@@ -70,8 +82,14 @@ public class RegisterServlet extends HttpServlet {
     response.getWriter().println("<p>Username: " + username + "</p>");
     response.getWriter().println("<p>Password: " + password + "</p>");
 
-    User user = new User(UUID.randomUUID(), username, BCrypt.hashpw(password, BCrypt.gensalt()), Instant.now());
+    UUID id = UUID.randomUUID();
+    Instant current = Instant.now();
+
+    User user = new User(id, username, BCrypt.hashpw(password, BCrypt.gensalt()), current);
     userStore.addUser(user);
+
+    UserProfile profile = new UserProfile(id, "No about me yet", "https://i.imgur.com/z4amwTY.png", null, current);
+    profileStore.addUserProfile(profile); 
 
     response.sendRedirect("/login");
   }
--- src/test/java/codeu/controller/ProfilePageServletTest.java
+++ src/test/java/codeu/controller/ProfilePageServletTest.java
@@ -1,13 +1,24 @@
 package codeu.controller;
 
 import java.io.IOException;
+import java.util.UUID;
+import java.util.Map;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import java.time.Instant;
+import java.util.Map;
+import codeu.model.data.User;
+import codeu.model.data.UserProfile;
+import codeu.model.store.basic.UserStore;
+import codeu.model.store.basic.UserProfileStore;
+import java.util.HashMap;
+
 
 
 public class ProfilePageServletTest {
@@ -16,18 +27,44 @@ public class ProfilePageServletTest {
   private HttpServletRequest mockRequest;
   private HttpServletResponse mockResponse;
   private RequestDispatcher mockRequestDispatcher;
+  private HttpSession mockSession;
+  private UserStore mockUserStore;
+  private UserProfileStore mockProfileStore;
+
+
   @Before
   public void setup() {
     profilePageServlet = new ProfilePageServlet();
     mockRequest = Mockito.mock(HttpServletRequest.class);
+    mockSession = Mockito.mock(HttpSession.class);
     mockResponse = Mockito.mock(HttpServletResponse.class);
     mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
     Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/profilepage.jsp")).thenReturn(mockRequestDispatcher);
+    mockUserStore = Mockito.mock(UserStore.class);
+    mockProfileStore = Mockito.mock(UserProfileStore.class);
   }
 
   @Test
   public void testDoGet() throws IOException, ServletException {
-    profilePageServlet.doGet(mockRequest, mockResponse);
+    Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+    profilePageServlet.setUserStore(mockUserStore);
+    profilePageServlet.setUserProfileStore(mockProfileStore);
+    Mockito.when(mockSession.getAttribute("user")).thenReturn("test_username");
+
+    UUID fakeUUID = UUID.randomUUID();
+    Instant current = Instant.now();
+    Map<String,String> fakeInterests = new HashMap<String, String>();
+    fakeInterests.put("test_category", "test_interest");
+    User fakeuser = new User(fakeUUID, "test_username", "password", current);
+    UserProfile fakeprofile = new UserProfile(fakeUUID, "test_about", "picture_url", fakeInterests, current);
+
+    Mockito.when(mockUserStore.getUser("test_username")).thenReturn(fakeuser); //
+    Mockito.when(mockProfileStore.getUserProfile(fakeUUID)).thenReturn(fakeprofile);
+
+    profilePageServlet.doGet(mockRequest, mockResponse); //
+    Mockito.verify(mockRequest).setAttribute("interests", fakeInterests);
+    Mockito.verify(mockRequest).setAttribute("aboutMe", "test_about");
+    Mockito.verify(mockRequest).setAttribute("profilePic", "picture_url");
 
     Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
   }
